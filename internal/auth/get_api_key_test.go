33575debc741c1ae58cf4eba6be0a95b90c1fa40
mattm3192 <mattm3192@gail.com>
2026-08-10 23:44:40 -0500

    testing with broken tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name       string
+		headers    http.Header
+		wantKey    string
+		wantErrMsg string
+	}{
+		{
+			name:       "No Auth Header Included",
+			headers:    http.Header{},
+			wantKey:    "",
+			wantErrMsg: "no authorization header included",
+		},
+		{
+			name:       "Malformed Auth Header",
+			headers:    http.Header{"Authorization": []string{"Bearer secret123"}},
+			wantKey:    "",
+			wantErrMsg: "malformed authorization header",
+		},
+		{
+			name:       "valid test",
+			headers:    http.Header{"Authorization": []string{"ApiKey secret123"}},
+			wantKey:    "secret123",
+			wantErrMsg: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			api, err := GetAPIKey(tt.headers)
+			if err == nil && !strings.Contains(err.Error(), tt.wantErrMsg) {
+				t.Fatalf("expected: %v, got: %v", tt.wantErrMsg, err.Error())
+			}
+			if api != tt.wantKey {
+				t.Fatalf("expected: %v, got: %v", tt.wantKey, api)
+			}
+
+		})
+	}
+}
